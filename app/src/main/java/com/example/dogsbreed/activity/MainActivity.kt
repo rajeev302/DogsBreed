6a277e5ac05caf495127036e259b943bc11221e7
USERD843 <userd843@administrators-MacBook-Pro.local>
2020-12-19 21:54:49 +0530

    breed list shown on ui

--- app/src/main/java/com/example/dogsbreed/activity/MainActivity.kt
+++ app/src/main/java/com/example/dogsbreed/activity/MainActivity.kt
@@ -3,26 +3,55 @@ package com.example.dogsbreed.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.ProgressBar
+import android.widget.TextView
 import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.dogsbreed.R
+import com.example.dogsbreed.adapter.BreedListAdapter
 import com.example.dogsbreed.model.breedlist.BreedListResponseModel
 import com.example.dogsbreed.viewmodel.BreedListViewModel
+import kotlinx.android.synthetic.main.activity_main.*
 
 private lateinit var viewmodel: BreedListViewModel
+private lateinit var breedRecyclerView: RecyclerView
+private lateinit var breedAdapter: BreedListAdapter
+private lateinit var noDataAvailable: TextView
+private lateinit var progressBar: ProgressBar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         viewmodel = ViewModelProvider(this).get(BreedListViewModel::class.java)
+
+        setupUi()
+        fireApiCall()
     }
 
-    override fun onResume() {
-        super.onResume()
-        viewmodel.getBreedList(success = {
-                                         Log.d("test_dogs_breed_data", "data is ${viewmodel.breedList}")
-            Toast.makeText(this, "test_dogs_breed_data ${viewmodel.breedList}", Toast.LENGTH_LONG).show()
-        }, failure = {})
+    private fun fireApiCall(){
+        viewmodel.getBreedList({
+            breedAdapter.notifyDataSetChanged()
+            progressBar.visibility = View.GONE
+            noDataAvailable.visibility = View.GONE
+            breedRecyclerView.visibility = View.VISIBLE
+        }, {
+            progressBar.visibility = View.GONE
+            noDataAvailable.visibility = View.VISIBLE
+            breedRecyclerView.visibility = View.GONE
+        })
+    }
+
+    private fun setupUi(){
+        breedRecyclerView = dogs_breed_recycler_view
+        noDataAvailable = no_data_available
+        progressBar = progress_bar
+        breedAdapter = BreedListAdapter(this, viewmodel.breedList)
+        val layoutManager = LinearLayoutManager(this)
+        breedRecyclerView.layoutManager = layoutManager
+        breedRecyclerView.adapter = breedAdapter
     }
 }
--- app/src/main/java/com/example/dogsbreed/adapter/BreedListAdapter.kt
+++ app/src/main/java/com/example/dogsbreed/adapter/BreedListAdapter.kt
@@ -1,4 +1,34 @@
 package com.example.dogsbreed.adapter
 
-class BreedListAdapter {
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.dogsbreed.R
+
+class BreedListAdapter(
+    val context: Context,
+    val dataSet: MutableList<String>
+) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    override fun getItemCount(): Int {
+        return dataSet.count()
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        val issueHolder = holder as BreedListViewHolder
+        issueHolder.breedTextView.text = dataSet[position]
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        val itemView = LayoutInflater.from(parent.context)
+            .inflate(R.layout.layout_breed_list_row, parent, false)
+        return BreedListViewHolder(itemView)
+    }
+
+    inner class BreedListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val breedTextView: TextView = itemView.findViewById(R.id.breed_name_text_view)
+    }
 }
